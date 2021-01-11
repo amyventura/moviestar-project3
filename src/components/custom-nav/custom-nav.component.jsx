39572fb34dc5ea96ffb0db9456fd3ecd92dfb927
Angelica Reyes <areyes1293@gmail.com>
2021-01-11 14:58:04 -0800

    update node modules and logo

--- src/components/custom-nav/custom-nav.component.jsx
+++ src/components/custom-nav/custom-nav.component.jsx
@@ -1,5 +1,5 @@
 import React,{useEffect,useState} from 'react'
-
+import logo from './Moviestar-logo-white.png'
 import './custom-nav.styles.css'
 
 
@@ -28,16 +28,15 @@ const [scroll,handleScroll]=useState(false);
         <div className={`nav ${scroll&&'nav_black'}`}>
             
             <Navbar collapseOnSelect expand="lg" bg="transparent" variant="dark" style={{width:'100vw'}}>
-    <Navbar.Brand href="/"><img src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" alt="logo" className='logo'/> </Navbar.Brand>
+    <Navbar.Brand href="/"><img class="logo" src={logo} alt="Logo" /> </Navbar.Brand>
     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
     <Navbar.Collapse id="responsive-navbar-nav">
       <Nav className="mr-auto">
         <Nav.Link href="/home">Home</Nav.Link>
         <Nav.Link href="#Favorites">Favorites</Nav.Link>
         <Nav.Link href="#Watch later">Watch Later</Nav.Link>
-
-        {/* <Nav.Link href="#Horror">Horror</Nav.Link>
-        <Nav.Link href="#Drama">Drama
+        <Nav.Link href="#Chat">Chat</Nav.Link>
+        {/* <Nav.Link href="#Drama">Drama
 </Nav.Link>
         <Nav.Link href="#Comedy">Comedy</Nav.Link> */}
 
